07a8ac3c4f6acd627feba8530053507eca3fe481
Pash10g <pash10g@gmail.com>
2018-11-06 13:26:32 +0200

    Update launch.sh

--- task4/launch.sh
+++ task4/launch.sh
@@ -5,7 +5,9 @@ OPSLAUNCH_SCRIPT=`readlink /usr/local/bin/opslaunch`
 
 OPSLAUNCH_DIR=`dirname $OPSLAUNCH_SCRIPT`
 
+opslaunch --remove --dir ../task3/data
+
 echo "Found opslaunch location : ${OPSLAUNCH_DIR}"
 echo "No deployment for this task :) ... Perhaps the `pwd`/diagnostics.zip file can help. "
 echo "Once completed please input the required change to be provided to the customer (hint: answer format <PROPERTY>:<VALUE>=><PROPERTY>:<VALUE>... Good luck!"
-echo "Hint: To ssh into the servers, from this directory (`pwd`) run \"opslaunch ssh --server <SERVER_NAME>\""
+echo "Hint: you can use the 00389925 case for https://proactive.corp.mongodb.com/diagnostics"
